26b63f252fea7d1719cace49d72c2aa37c1718b0
rzamzam <126548575+rzamzam@users.noreply.github.com>
2023-12-20 16:18:25 +0700

    Add files via upload

---
+++ incremental.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+def insert_new_territory(conn, territory_id, territory_name):
+    try:
+        # Create a cursor
+        cursor = conn.cursor()
+
+        # Insert new values into dimterritory
+        cursor.execute("INSERT INTO dimterritory (territoryid, name) VALUES (%s, %s)", (territory_id, territory_name))
+
+        # Commit the changes
+        conn.commit()
+
+        print("New territory added successfully!")
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+    finally:
+        # Close the cursor
+        cursor.close()
+
+# Example usage
+postgres_conn_string = "dbname=staging2 user=postgres password=rayhanzam25 host=localhost"
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(postgres_conn_string)
+
+# Insert new territory
+insert_new_territory(conn, 12, "Surabaya")
+
+# Close the connection
+conn.close()
